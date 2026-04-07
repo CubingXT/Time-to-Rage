6fbae02702199386f0ed6559587e745ce562df60
CubingXT <tommyebs@icloud.com>
2026-04-06 21:18:38 -0500

    Mod runs at 30% for testing purposes

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,74 @@
-// Include headers and bring namespaces to the current scope
+// Include the Geode header and other headers
+#include <filesystem>
 #include <Geode/Geode.hpp>
-#include <Geode/modify/PlayLayer.hpp>
+#include <Geode/binding/FMODAudioEngine.hpp>
+
+// Includes headers for layers being modified, hooked, or using methods
+#include <Geode/modify/PlayerObject.hpp>
+#include <Geode/binding/PlayLayer.hpp>
+
+// Includes dependencies
 #include <cvolton.level-id-api/include/EditorIDs.hpp>
 
+// Brings all Geode namespaces to the current scope
 using namespace geode::prelude;
 
-// Modifwying the play layer
-class $modify(PlayerHasDied, PlayLayer) {
+// Modifying PlayerObject
+class $modify(TimeToRageDeath, PlayerObject) {
 
-    // Hook destroyPlayer functionw
-    void destroyPlayer(PlayerObject* player, GameObject* obj) {
-        
+    // Hook playerDestroyed function
+    void playerDestroyed(bool p0) {
 
-        PlayLayer::destroyPlayer(player, obj);
+        // Declares playLayer function for use as a variable
+        auto playLayer = PlayLayer::get();
 
-        float percent = this->getCurrentPercent();
-        auto level = PlayLayer::get()->m_level;
-        int levelId = EditorIDs::getID(level);
-        if (percent > 2) log::info("Died at {}%", percent);
-        log::info("on level {}", levelId);
-    } 
+        // Checking if playtesting and terminates if it is
+        if (LevelEditorLayer::get()) {
+
+            // Call original playerDestroyed function as we are in playtesting mode
+            PlayerObject::playerDestroyed(p0);
+
+        // Scripts that run if not in playtesting    
+        } else if (playLayer) {
+
+            // Getting booleans to check if mod should run
+            bool levelIsPlatformer = playLayer->m_isPlatformer;
+            bool fromStartPosition = playLayer->m_isTestMode;
+            bool playingInPracticeMode = playLayer->m_isPracticeMode;
+            
+            // Scripts to run on player death
+
+            // Get death certificate
+            auto level = playLayer->m_level;
+            int levelId = level ? EditorIDs::getID(level) : -1;
+            float percent = playLayer->getCurrentPercent();
+
+            // Call original playerDestroyed function
+            PlayerObject::playerDestroyed(p0);
+
+            // Scripts to run after player death
+            if (!levelIsPlatformer && !fromStartPosition && !playingInPracticeMode) {
+                if (percent >= 30) {
+                    auto path = Mod::get()->getResourcesDir() / "ahhhSoundEffect.wav";
+                    
+                    // Check if file actually exists
+                    if (std::filesystem::exists(path)) {
+                        auto pathStr = path.string();
+                        FMODAudioEngine::sharedEngine()->preloadEffect(pathStr);
+                        Loader::get()->queueInMainThread([pathStr, playLayer]() {
+                            FMODAudioEngine::sharedEngine()->playEffect(pathStr);
+                            playLayer->pauseGame(false);
+                            FMODAudioEngine::sharedEngine()->resumeAllEffects();
+                        });   
+                    }
+                }
+            }
+
+        // Scripts that run if something weird happens
+        } else {
+
+            // Call original playerDestroyed function as something weird happened
+            PlayerObject::playerDestroyed(p0);
+        }  
+    }
 };
